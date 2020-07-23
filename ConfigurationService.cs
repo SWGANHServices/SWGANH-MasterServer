b4a6e4aae18fd1e0f1c083df92eafce3223eb4de
Obi-Two <johnroyuk@gmail.com>
2020-07-23 09:25:33 +0100

    Start on Config Services

---
+++ ConfigurationService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWGANH_MasterServer
+{
+    public class ConfigurationService
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public static ConfigurationService CreateInstance()
+        {
+            return CreateInstance((s) => { });
+        }
+
+        public static ConfigurationService CreateInstance(Action<IServiceCollection> handler)
+        {
+            var instance = new ConfigurationService();
+
+            var descriptors = CreateDefaultServiceDescriptors();
+            handler(descriptors);
+
+            instance.ServiceProvider = descriptors.BuildServiceProvider();
+            return instance;
+        }
+
+        private static IServiceCollection CreateDefaultServiceDescriptors()
+        {
+            var Configuration = new ConfigurationBuilder()
+                .AddJsonFile("Config.json")
+                .Build();
+            IServiceCollection serviceDescriptors = new ServiceCollection();
+            serviceDescriptors.AddLogging(b => b.AddNLog());
+            serviceDescriptors.AddSingleton<IConfigurationRoot>(Configuration);
+
+            return serviceDescriptors;
+
+        }
+    }
+}
---
+++ Service/ServiceModels/ClientConnection.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SWGANH_MasterServer.Service.ServiceModels
+{
+    public class ClientConnection
+    {
+        public Guid ConnectionId { get; set; }
+
+        private TcpClient tcpClient;
+
+        private readonly NetworkStream stream;
+
+        public BinaryReader Reader { get; }
+        public BinaryWriter Writer { get; }
+
+        public ClientConnection(Guid ConnectionId, TcpClient tcpClient)
+        {
+            this.ConnectionId = ConnectionId;
+            this.tcpClient = tcpClient;
+            this.stream = tcpClient.GetStream();
+            this.Reader = new BinaryReader(stream);
+            this.Writer = new BinaryWriter(stream);
+        }
+
+        public int AvailableBytes => tcpClient.Available;
+        public bool isConnected => tcpClient.Connected;
+    }
+}
